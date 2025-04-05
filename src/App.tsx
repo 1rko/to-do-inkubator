f282dd055c294a15b477b3de976277ed23db34e9
1rko <irkoabc@gmail.com>
2025-04-05 22:33:40 +0300

    Week3 Sprint 1 - Create Task, controlled input + Homework (children)

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,41 @@
 import './App.css'
 import {TodolistItem} from "./TodolistItem.tsx";
 import {useState} from "react";
+import {v1} from "uuid";
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
 
 export const App = () => {
     const tasks1: TaskType[] = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: false},
-        {id: 4, title: 'Redux', isDone: false},
-        {id: 5, title: 'Typescript', isDone: false},
-        {id: 6, title: 'RTK query', isDone: false},
-    ]
-
-    const tasks2: TaskType[] = [
-        /*{id: 1, title: 'Hello world', isDone: true},
-        {id: 2, title: 'I am Happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false},*/
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'ReactJS', isDone: false},
+        {id: v1(), title: 'Redux', isDone: false},
+        {id: v1(), title: 'Typescript', isDone: false},
+        {id: v1(), title: 'RTK query', isDone: false},
     ]
 
     const [tasks, setTasks] = useState(tasks1)
 
-    const deleteTask = (id: number) => {
+    const deleteTask = (id: string) => {
         setTasks(tasks.filter(task => task.id !== id))
     }
 
     const deleteAllTasks = () => {
         setTasks([])
+    }
+
+    const createTask = (title: string) => {
+        let newTask = {
+            id: v1(),
+            title: title,
+            isDone: false
+        }
+        setTasks([newTask, ...tasks])
     }
 
     return (
@@ -41,7 +45,19 @@ export const App = () => {
                 tasks={tasks}
                 deleteTask={deleteTask}
                 deleteAllTasks={deleteAllTasks}
+                createButtonClick={createTask}
             />
+            <TodolistItem
+                title="asfsdf"
+                tasks={tasks}
+                deleteTask={deleteTask}
+                deleteAllTasks={deleteAllTasks}
+                createButtonClick={createTask}
+            >
+                <div>
+                    <div>Many intresting information</div>
+                </div>
+            </TodolistItem>
             {/* <TodolistItem title="Songs" tasks={tasks2}/>
             <TodolistItem title="Books" tasks={tasks2}/>*/}
         </div>
